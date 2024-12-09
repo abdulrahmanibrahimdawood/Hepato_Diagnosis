d3ffc0e6b1e70e1e6666941e7bca4824cc4d252a
abdulrahmanibrahimdawood <abdoibrahim9111@gmail.com>
2024-12-09 16:59:52 +0200

    a

--- lib/widget/custom_buttom_app_bar.dart
+++ lib/widget/custom_buttom_app_bar.dart
@@ -1,111 +1,3 @@
-// import 'package:flutter/material.dart';
-// import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-// import 'package:hepato_diagnosis/constant.dart';
-
-// class CustomBottomAppBarIcons extends StatelessWidget {
-//   const CustomBottomAppBarIcons({
-//     super.key,
-//     required this.text,
-//     required this.icon,
-//     required this.onPressed,
-//     required this.isActive,
-//   });
-
-//   final String text;
-//   final IconData icon;
-//   final void Function()? onPressed;
-//   final bool isActive;
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Column(
-//       children: [
-//         IconButton(
-//           onPressed: onPressed,
-//           icon: FaIcon(
-//             icon,
-//             color: isActive ? kPrimaryColor : Colors.grey,
-//             size: 28,
-//           ),
-//         ),
-//         Text(
-//           text,
-//           style: TextStyle(
-//               color: isActive ? kPrimaryColor : Colors.grey,
-//               fontWeight: FontWeight.bold),
-//         ),
-//       ],
-//     );
-//   }
-// }
-
-// class BodyBottomAppBar extends StatefulWidget {
-//   const BodyBottomAppBar({super.key});
-
-//   @override
-//   State<BodyBottomAppBar> createState() => _BodyBottomAppBarState();
-// }
-
-// class _BodyBottomAppBarState extends State<BodyBottomAppBar> {
-//   int activeIndex = 0;
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Container(
-//       width: MediaQuery.of(context).size.width,
-//       decoration: const BoxDecoration(),
-//       child: Padding(
-//         padding: const EdgeInsets.only(bottom: 15, left: 20, right: 20),
-//         child: Row(
-//           mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//           children: [
-//             CustomBottomAppBarIcons(
-//               onPressed: () {
-//                 setState(() {
-//                   activeIndex = 0;
-//                 });
-//               },
-//               icon: FontAwesomeIcons.houseChimneyMedical,
-//               text: 'Home',
-//               isActive: activeIndex == 0,
-//             ),
-//             CustomBottomAppBarIcons(
-//               onPressed: () {
-//                 setState(() {
-//                   activeIndex = 1;
-//                 });
-//               },
-//               icon: Icons.analytics_sharp,
-//               text: 'Analysis',
-//               isActive: activeIndex == 1,
-//             ),
-//             CustomBottomAppBarIcons(
-//               onPressed: () {
-//                 setState(() {
-//                   activeIndex = 2;
-//                 });
-//               },
-//               icon: Icons.question_answer,
-//               text: 'Questions',
-//               isActive: activeIndex == 2,
-//             ),
-//             CustomBottomAppBarIcons(
-//               onPressed: () {
-//                 setState(() {
-//                   activeIndex = 3;
-//                 });
-//               },
-//               icon: FontAwesomeIcons.stethoscope,
-//               text: 'Doctors',
-//               isActive: activeIndex == 3,
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
-
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:hepato_diagnosis/constant.dart';
@@ -133,19 +25,31 @@ class _MainPageState extends State<CustomBottomAppBarIcons> {
 
   static const List<BottomNavigationBarItem> _navigationItems = [
     BottomNavigationBarItem(
-      icon: FaIcon(FontAwesomeIcons.house),
+      icon: FaIcon(
+        FontAwesomeIcons.house,
+        size: 28,
+      ),
       label: 'Home',
     ),
     BottomNavigationBarItem(
-      icon: FaIcon(FontAwesomeIcons.notesMedical),
+      icon: FaIcon(
+        FontAwesomeIcons.notesMedical,
+        size: 28,
+      ),
       label: 'Analysis',
     ),
     BottomNavigationBarItem(
-      icon: FaIcon(FontAwesomeIcons.clipboardQuestion),
+      icon: FaIcon(
+        FontAwesomeIcons.clipboardQuestion,
+        size: 28,
+      ),
       label: 'Questions',
     ),
     BottomNavigationBarItem(
-      icon: FaIcon(FontAwesomeIcons.userDoctor),
+      icon: FaIcon(
+        FontAwesomeIcons.userDoctor,
+        size: 28,
+      ),
       label: 'Doctors',
     ),
   ];
