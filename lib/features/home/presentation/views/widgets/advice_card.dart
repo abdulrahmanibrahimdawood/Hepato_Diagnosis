c6b0ca642c655c29bc53aa11ea72cb8398cffc27
abdulrahmanibrahimdawood <abdoibrahim9111@gmail.com>
2025-02-26 21:31:23 +0200

    edit ui

--- lib/features/home/presentation/views/widgets/advice_card.dart
+++ lib/features/home/presentation/views/widgets/advice_card.dart
@@ -8,59 +8,62 @@ class AdviceCard extends StatelessWidget {
     super.key,
   });
   AdviceModel advice;
+
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(right: 10, left: 10),
       child: Container(
-          height: 330,
-          decoration: BoxDecoration(
-            color: AppColor.kPrimaryColor.withValues(alpha: .5),
-            borderRadius: BorderRadius.circular(10),
-          ),
-          width: MediaQuery.of(context).size.width * 0.90,
-          child: Padding(
-            padding: const EdgeInsets.all(12),
-            child: Column(
-              children: [
-                const SizedBox(
-                  height: 8,
-                ),
-                Text(
-                  advice.title,
-                  style: const TextStyle(
-                      fontWeight: FontWeight.bold, fontSize: 19),
-                  textAlign: TextAlign.center,
-                  maxLines: 1,
-                  overflow: TextOverflow.ellipsis,
-                ),
-                const SizedBox(height: 15),
-                Image.network(
+        height: 330,
+        decoration: BoxDecoration(
+          color: AppColor.kPrimaryColor.withValues(alpha: .5),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        width: MediaQuery.of(context).size.width * 0.90,
+        child: Padding(
+          padding: const EdgeInsets.all(12),
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 8,
+              ),
+              Text(
+                advice.title,
+                style:
+                    const TextStyle(fontWeight: FontWeight.bold, fontSize: 19),
+                textAlign: TextAlign.center,
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+              ),
+              const SizedBox(height: 15),
+              SizedBox(
+                height: 150,
+                width: 150,
+                child: Image.network(
                   advice.image,
-                  height: 150,
-                  width: 150,
                   fit: BoxFit.contain,
                   errorBuilder: (context, error, stackTrace) => const Icon(
                     Icons.image_not_supported,
                     color: Colors.grey,
-                    size: 50,
+                    size: 150,
                   ),
                 ),
-                const SizedBox(
-                  height: 12,
-                ),
-                Text(
-                  advice.description,
-                  textAlign: TextAlign.center,
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
-                  style: const TextStyle(
-                      fontSize: 14, fontWeight: FontWeight.w600),
-                ),
-              ],
-            ),
-          )),
+              ),
+              const SizedBox(
+                height: 12,
+              ),
+              Text(
+                advice.description,
+                textAlign: TextAlign.center,
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+                style:
+                    const TextStyle(fontSize: 14, fontWeight: FontWeight.w600),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
-//
