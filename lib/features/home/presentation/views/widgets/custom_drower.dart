0dcd62331adda445060840f1e6da146f85190bff
abdulrahmanibrahimdawood <abdoibrahim9111@gmail.com>
2025-04-03 12:57:55 +0200

    login shared prefrence

--- lib/features/home/presentation/views/widgets/custom_drower.dart
+++ lib/features/home/presentation/views/widgets/custom_drower.dart
@@ -1,3 +1,5 @@
+import 'package:e_commerce/constants.dart';
+import 'package:e_commerce/core/services/shared_preferences_singleton.dart';
 import 'package:e_commerce/core/utils/app_color.dart';
 import 'package:e_commerce/features/auth/presentation/views/signin_view.dart';
 import 'package:e_commerce/features/home/cubit/logout/logout_cubit.dart';
@@ -15,6 +17,7 @@ class CustomDrawer extends StatelessWidget {
       child: BlocConsumer<LogoutCubit, LogoutState>(
         listener: (context, state) {
           if (state is LogoutSuccess) {
+            SharedPrefs.setBoolLogin(kIsLogedin, false);
             Navigator.pushNamedAndRemoveUntil(
               context,
               SigninView.routeName,
